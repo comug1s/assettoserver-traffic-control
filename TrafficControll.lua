e3aded3241fad749731956ea8f76d0edf1375801
comug1s <ohsawa.shinnosuke@gmail.com>
2024-05-12 17:53:24 +0900

    Add files via upload

---
+++ TrafficControll.lua
@@ -0,0 +1,9 @@
+local sim = ac.getSim()
+
+ai_commands = {
+    {command='Extra.AiPerms.AiPerPlayerTargetCount',value=5,window=vec2(0,50)}
+}
+
+function setAiPerPlayerTargetCount(id,duration)
+
+end
